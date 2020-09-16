99b9d528bc117155ed3da6bcc72d5b3092f4aad5
chidimi <daichi.imai@hotmail.com>
2020-09-17 01:56:11 +0900

    todo sample

---
+++ src/frontend/views/newTask.ts
@@ -0,0 +1,16 @@
+import { Component, Vue } from "vue-property-decorator";
+import { ITask } from "../../model/task/task";
+import { postTask } from "../api/task";
+
+@Component
+export class NewTaskView extends Vue {
+
+    private text: string = "";
+
+    public async clickAddButton(): Promise<void> {
+        const task: ITask = { id: 0, text: this.text };
+        await postTask(task);
+        this.text = "";
+        this.$emit("updateTaskList");
+    }
+}
---
+++ src/frontend/views/taskList.ts
@@ -0,0 +1,13 @@
+import { Component, Vue } from "vue-property-decorator";
+import { ITask } from "../../model/task/task";
+import { loadTasks, postTaskDone } from "../api/task";
+
+@Component
+export class TaskListView extends Vue {
+    private tasks: ITask[] = [];
+
+    public async loadTasks(): Promise<void> {
+        const tasks = await loadTasks();
+        this.tasks = tasks;
+    }
+}
